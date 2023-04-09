4d52b9aa8fbc98fdb3c8178db08ca95fae7c8d4a
Ruslan <trud555@ukr.net>
2023-04-09 17:48:34 +0300

    zz9

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ import './Button.css'
 
 const Button = (props) => {
     return (
-        <button {...props} className={'button' + props.className}/>
+        <button {...props} className={'button ' + props.className}/>
     );
 };
 
